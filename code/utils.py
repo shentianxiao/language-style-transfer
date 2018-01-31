8fb22dd04c8114e045e5fe1edcb616711b158598
Tianxiao Shen <tianxiao@30-10-29.wireless.csail.mit.edu>
2018-01-30 21:42:30 -0500

    noisy input

--- code/utils.py
+++ code/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+import random
 
 def strip_eos(sents):
     return [sent[:sent.index('<eos>')] if '<eos>' in sent else sent
@@ -29,7 +31,18 @@ def reorder(order, _x):
         x[i] = a
     return x
 
-def get_batch(x, y, word2id, min_len=5):
+# noise model from paper "Unsupervised Machine Translation Using Monolingual Corpora Only"
+def noise(x, unk, word_drop=0.1, k=3):
+    n = len(x)
+    for i in range(n):
+        if random.random() < word_drop:
+            x[i] = unk
+
+    # slight shuffle such that |sigma[i]-i| <= k
+    sigma = (np.arange(n) + (k+1) * np.random.rand(n)).argsort()
+    return [x[sigma[i]] for i in range(n)]
+
+def get_batch(x, y, word2id, noisy=False, min_len=5):
     pad = word2id['<pad>']
     go = word2id['<go>']
     eos = word2id['<eos>']
@@ -42,7 +55,8 @@ def get_batch(x, y, word2id, min_len=5):
         sent_id = [word2id[w] if w in word2id else unk for w in sent]
         l = len(sent)
         padding = [pad] * (max_len - l)
-        rev_x.append(padding + sent_id[::-1])
+        _sent_id = noise(sent_id, unk) if noisy else sent_id
+        rev_x.append(padding + _sent_id[::-1])
         go_x.append([go] + sent_id + padding)
         x_eos.append(sent_id + [eos] + padding)
         weights.append([1.0] * (l+1) + [0.0] * (max_len-l))
@@ -55,7 +69,7 @@ def get_batch(x, y, word2id, min_len=5):
             'size':       len(x),
             'len':        max_len+1}
 
-def get_batches(x0, x1, word2id, batch_size):
+def get_batches(x0, x1, word2id, batch_size, noisy=False):
     if len(x0) < len(x1):
         x0 = makeup(x0, len(x1))
     if len(x1) < len(x0):
@@ -75,7 +89,7 @@ def get_batches(x0, x1, word2id, batch_size):
     while s < n:
         t = min(s + batch_size, n)
         batches.append(get_batch(x0[s:t] + x1[s:t],
-            [0]*(t-s) + [1]*(t-s), word2id))
+            [0]*(t-s) + [1]*(t-s), word2id, noisy))
         s = t
 
     return batches, order0, order1
