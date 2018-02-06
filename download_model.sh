e2865cbfe4172fb3fdf9b763c2e5062604d81ad6
Tianxiao Shen <tianxiao@30-10-66.wireless.csail.mit.edu>
2018-02-06 15:53:13 -0500

    download trained model

---
+++ download_model.sh
@@ -0,0 +1,8 @@
+mkdir -p model
+
+dir="http://people.csail.mit.edu/tianxiao/language-style-transfer/model/"
+
+wget ${dir}yelp.d100.emb.txt -P model/
+wget ${dir}model.data-00000-of-00001 -P model/
+wget ${dir}model.index -P model/
+wget ${dir}model.meta -P model/
